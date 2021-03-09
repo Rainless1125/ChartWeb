a3704c68f86758636b3e85320283d5c3d0a03241
Rainless1125 <ah247517m@gmail.com>
2021-03-09 23:42:08 +0800

    python to json test2

--- py/GetWebData.py
+++ py/GetWebData.py
@@ -3,20 +3,20 @@ if __name__ == '__main__':
     import json
     from datetime import date
     from pyquery import PyQuery as pq
+    list_data = []
     my_dict = {}
-    rank_table = {}
     today = date.today()
     data = pq(url="https://www.gamer.com.tw/",headers={'user-agent': 'Mozilla/5.0'})
     table = data("#hotboard")
     df = pd.read_html(table.html())
     for i in range(0,len(df)):#two table
         for row in range(1,df[i].shape[0]):#shape get 11
-            rank_table['name','num'] = [df[i][1][row],df[i][2][row]]
-            #my_dict[df[i][1][row]] = [df[i][2][row],str(today)]
-    my_dict['date'] = str(today)
-    my_dict['data'] = rank_table
-    with open('rank.json', 'w', encoding='utf-8') as f:
-        json.dump(my_dict, f,ensure_ascii=False, indent = 4)#避免中文轉成ascii
-    j = json.loads('rank.json')
-    print(j)
-            
+        	my_dict['name'] = df[i][1][row]
+        	my_dict['num'] = df[i][2][row]
+        	my_dict['date'] = str(today)
+        	list_data.append(my_dict.copy())
+            #my_dict['name'] = df[i][1][row]
+            #rank_table['name','num'] = [df[i][1][row],df[i][2][row]]
+            #my_dict[df[i][1][row]] = df[i][2][row]
+    with open('rank.json', 'a', encoding='utf-8') as f:
+        json.dump(list_data, f,ensure_ascii=False, indent = 4)# ensure_ascii=False 避免中文轉成ascii      
